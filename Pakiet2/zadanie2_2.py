11e6558c2e4ffa7919a1dab7959fd70e3c3ac887
jkrasowski21@gmail.com <Anakin21@aa>
2024-04-22 01:23:51 +0200

    2-4

--- Pakiet2/zadanie2_2.py
+++ Pakiet2/zadanie2_2.py
@@ -1,3 +1,6 @@
-test
+from itertools import combinations
+def wszystkie_kombinacje(elementy):
+    return list(combinations(elementy, 2))
 
-zadanie2_zadanie2_zadanie2
+przykladowa_lista = [1, 2, 3, 4]
+print(wszystkie_kombinacje(przykladowa_lista)) 
